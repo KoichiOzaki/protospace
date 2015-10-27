7e553a175c56ba8d1283d678fb1e7d04ce60d486
尾崎皓一 <cabits@kabushikikaishacabits-no-MacBook-Pro.local>
2015-10-27 14:20:32 +0900

    Delete the "/user/new"-routing and a view because this function modify the devise gem.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :top, only: :index
   resources :prototypes, only: [:index, :new]
-  resources :users, only: [:index, :new, :edit]
+  resources :users, only: [:index, :edit]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
