0aa5f828786e3c03d3370537cb5db2226af7b85c
尾崎皓一 <cabits@kabushikikaishacabits-no-MacBook-Pro.local>
2015-11-17 20:06:39 +0900

    Add the function of posting a prototype.

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,30 @@
 class PrototypesController < ApplicationController
   def index
   end
+  
   def new
+    @prototype = Prototype.new
+    @prototype.prototype_images.build
   end
+  
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path, notice: "Post the new PROTO successfully."
+    else
+      render :new
+    end
+
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(
+      :catchcopy,
+      :concept,
+      :title,
+      prototype_images_attributes: [:id, :image]
+      ).merge(user_id: current_user.id)
+  end
+
 end
