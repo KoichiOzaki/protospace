37ae2cb739a83aeac3861695188f6f21e339722b
尾崎皓一 <cabits@kabushikikaishacabits-no-MacBook-Pro.local>
2015-11-17 20:00:47 +0900

    Add the validation to Prototype model.

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,4 +2,7 @@ class Prototype < ActiveRecord::Base
   #association
   has_many :prototype_images , dependent: :destroy
   belongs_to :user
+
+  #validation
+  validates_presence_of :title, :catchcopy, :concept
 end
