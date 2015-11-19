e73c251898352ab1ea563ef0f6886ab4f438cb55
尾崎皓一 <cabits@kabushikikaishacabits-no-MacBook-Pro.local>
2015-11-19 18:05:49 +0900

    Edit the process of PrototypeImage model using "enum" method.

--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -3,4 +3,6 @@ class PrototypeImage < ActiveRecord::Base
   belongs_to :prototype
   #mount a uploader file of carrierwave
   mount_uploader :image, PrototypeImageUploader
+  #enum
+  enum pr_flag: %i(sub top)
 end
