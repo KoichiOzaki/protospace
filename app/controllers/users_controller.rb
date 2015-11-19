926b8446cb84351324b6a003e299efd97a36a538
尾崎皓一 <cabits@kabushikikaishacabits-no-MacBook-Pro.local>
2015-11-19 13:00:46 +0900

    Edit user's my page because add the user's info & use's prototype.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
   def show
+    @prototypes = current_user.prototypes.includes(:prototype_images)
   end
+
   def edit
   end
+
   def update
     if current_user.update(update_params)
       redirect_to root_path, notice: "Update profile successfully."
@@ -10,6 +13,7 @@ class UsersController < ApplicationController
       render :edit
     end
   end
+
   private
   def update_params
     params.require(:user).permit(:email, :member, :profile, :works, :username, :avatar)
