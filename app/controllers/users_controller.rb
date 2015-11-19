214eaab2c8cc50f6bbb204a625b3aac1e26e884e
尾崎皓一 <cabits@kabushikikaishacabits-no-MacBook-Pro.local>
2015-11-19 19:59:31 +0900

    Fix a bug that is displaying wrong user's page at users#show.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: [:show, :edit, :update]
+  before_action :redirect_to_top, only: [:edit, :update]
+
   def show
-    @prototypes = current_user.prototypes.page(params[:page]).eager_load(:prototype_images)
+    @prototypes = @user.prototypes.page(params[:page]).eager_load(:prototype_images)
   end
 
   def edit
+
   end
 
   def update
@@ -17,5 +21,13 @@ class UsersController < ApplicationController
   private
   def update_params
     params.require(:user).permit(:email, :member, :profile, :works, :username, :avatar)
+  end
+
+  def load_user
+    @user = User.find(params[:id])
+  end
+
+  def redirect_to_top
+    redirect_to root_path unless @user == current_user
   end
 end
