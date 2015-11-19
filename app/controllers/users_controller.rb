c9dc471ce7d15b961b4aa13c06efde30c98a48ed
尾崎皓一 <cabits@kabushikikaishacabits-no-MacBook-Pro.local>
2015-11-19 20:51:19 +0900

    Edit a before_action at users_controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :load_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:edit, :update]
   before_action :redirect_to_top, only: [:edit, :update]
 
   def show
@@ -22,7 +23,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:email, :member, :profile, :works, :username, :avatar)
   end
 
-  def load_user
+  def set_user
     @user = User.find(params[:id])
   end
 
