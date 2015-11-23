8dc56f1b81c7d589013526a8a260275e6616df59
尾崎皓一 <cabits@kabushikikaishacabits-no-MacBook-Pro.local>
2015-11-24 02:29:12 +0900

    Add the routings "like#create" & "like#destroy".

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'top#index'
+  post 'like/:prototype_id' => 'likes#create', as: 'like'
+  delete 'like/:prototype_id' => 'likes#destroy', as: 'destroy_like'
 
   resources :top, only: :index
   resources :prototypes, except: [:index] do
