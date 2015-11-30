4f46cddab2628052d296565dec8b4db312c3621d
尾崎皓一 <cabits@kabushikikaishacabits-no-MacBook-Pro.local>
2015-11-30 17:33:35 +0900

    Edit the routing and prefix using "resources" method.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   def create
-    Like.create(prototype_id: params[:prototype_id], user_id: current_user.id)
-    @prototype = Prototype.find(params[:prototype_id])
+    Like.create(prototype_id: params[:id], user_id: current_user.id)
+    @prototype = Prototype.find(params[:id])
   end
 
   def destroy
-    current_user.likes.find_by(prototype_id: params[:prototype_id]).destroy
-    @prototype = Prototype.find(params[:prototype_id])
+    current_user.likes.find_by(prototype_id: params[:id]).destroy
+    @prototype = Prototype.find(params[:id])
   end
 end
